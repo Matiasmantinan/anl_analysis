ff5a96b7bebaabeabef7e5aeb52eabcb9dbe70ff
TateFlicker <tateflicker@uchicago.edu>
2023-07-12 09:42:45 -0500

    test

--- mass/mass_measurement.py
+++ mass/mass_measurement.py
@@ -29,6 +29,7 @@ def add_date_folder():
 
 
 def main():
+    print("hello")
     path = add_date_folder()
     file_name = "mass_measurement_"+datetime.now().strftime("%m_%d_%y")+".csv"
     output_file = Path(path,file_name)
